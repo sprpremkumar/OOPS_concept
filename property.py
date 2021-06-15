65cb6778f57e36f3102b42c0a7ffe2079ebe1286
Prem Kumar R <74664800+sprpremkumar@users.noreply.github.com>
2021-06-15 17:09:39 +0530

    Update property.py

--- property.py
+++ property.py
@@ -1,16 +1,12 @@
-class myage:
-    def __init__(self):
-        self.__age=0
+class myname:
+    def __init__(self,name):
+        self.name=name
+    @property
+    def urname(self):
+        print( self.name)
+    @urname.setter
+    def urname(self,n):
+        self.name=n
 
-    def get_age(self):#getter method
-        return self.__age
-
-    def set_age(self,a):#setter method
-        self.__age=a
-
-prem_age=myage()
-
-prem_age.set_age(21) #setting the age
-
-print(prem_age.get_age())# getting the age
-
+name=myname("prem")
+name.urname
