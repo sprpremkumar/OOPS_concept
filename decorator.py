1c604c0a0657a21985797b2dc151021b6525f4dc
Prem Kumar R <74664800+sprpremkumar@users.noreply.github.com>
2021-06-15 17:49:15 +0530

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,15 +1,13 @@
-def divide(a,b):
-    print(a/b)
-
 def decorator(f):
-
     def newrule(a,b):
         if a<b:
             a,b=b,a
         return f(a,b)
     return newrule
 
-divide=decorator(divide)
-a=int(input("Enter the num1 :"))
-b=int(input("Enter the num2 :"))
-divide(a,b)
+
+@decorator
+def div(a,b):
+    print(a/b)
+
+div(5,7)
